ab95667759025c108813923c6cf0aa9e1f1c6bd5
jitendra <jitendrabe102@gmail.com>
2023-06-29 10:10:08 +0530

    two heading added in app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>Hello</h2>
+        <h1>Hello</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
